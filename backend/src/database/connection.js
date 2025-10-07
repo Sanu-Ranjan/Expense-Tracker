a336e4b5cc6c81b89a121de95764d5327827e834
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-07 10:36:38 +0530

    add connection to MySQL server

---
+++ backend/src/database/connection.js
@@ -0,0 +1,42 @@
+const config = require("config");
+const { Sequelize } = require("sequelize");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const database = new Sequelize(
+  config.get("mysqlDB.name"),
+  config.get("mysqlDB.user"),
+  config.get("mysqlDB.password"),
+  {
+    host: config.get("mysqlDB.host"),
+    port: config.get("mysqlDB.port"),
+    dialect: "mysql",
+    dialectOptions: isProd
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: true,
+            ca: config.get("mysqlDB.ssl").replace(/\\n/g, "\n"),
+          },
+        }
+      : {},
+  }
+);
+
+(async () => {
+  try {
+    await database.authenticate();
+    console.log("connection to database server has been extablished");
+    const [result] = await database.query(
+      "SHOW SESSION STATUS LIKE 'Ssl_cipher'"
+    );
+    console.log("SSL Cipher:", result[0]?.Value || "Not using SSL");
+  } catch (error) {
+    console.log("Unable to connect to data base: ", error);
+    process.exit(1);
+  }
+})();
+
+module.exports = {
+  database,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 require("../config/check");
 const { app } = require("./app");
+const { database } = require("./database/connection");
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on port:${port}`);
-});
+
+(async () => {
+  try {
+    await database.sync({ force: true });
+    app.listen(port, () => {
+      console.log(`listening on port:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
