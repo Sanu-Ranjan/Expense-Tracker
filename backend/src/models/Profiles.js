0ff3f482828bd5e408627ff4b142e64d2eebce39
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-07 14:18:54 +0530

    feat: profile model added  for profiles table in database

---
+++ backend/src/models/Profiles.js
@@ -0,0 +1,26 @@
+const { database } = require("../database/connection");
+const { DataTypes } = require("sequelize");
+
+const Profile = database.define("profiles", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  profileName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profilePurpose: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  additionalInfo: {
+    type: DataTypes.TEXT("tiny"),
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  Profile,
+};
