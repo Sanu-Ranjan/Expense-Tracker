98cd1bcd019661cd2dd2d5b8420d82ad1b738a14
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-08 11:21:21 +0530

    feat(fix): check ca cert only when not in production

--- backend/config/check.js
+++ backend/config/check.js
@@ -2,9 +2,14 @@ const config = require("config");
 
 let keys = Object.keys(config);
 let missing = false;
+let env = process.env.NODE_ENV === "development";
 
 for (let key of keys) {
   for (let configKey of Object.keys(config[key])) {
+    if (env && configKey === "caCert") {
+      console.log(`Skipping ca certificate`);
+      continue;
+    }
     if (!config[key][configKey]) {
       console.log(`fatal error: missing ${key} ${configKey}`);
       missing = true;
