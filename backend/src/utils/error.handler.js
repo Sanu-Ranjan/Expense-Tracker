e02480ac40c8992096dcd5c8060b08f32658e86c
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-09 21:05:02 +0530

    fix code

--- backend/src/utils/error.handler.js
+++ backend/src/utils/error.handler.js
@@ -1,6 +1,6 @@
 const { sendResponse } = require("./sendResponse");
 
-const errorHandler = async (req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const message = err.status === 500 ? "Internal server error" : err.message;
   console.log(err);
   res.status(err.status || 500).json(sendResponse.fail(message));
