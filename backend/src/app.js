48a183dd98a2d904ae77641de633cbe1ed8ef9be
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-09 19:30:33 +0530

    feat: use middlewares for parsing req, cookies and handle error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const { errorHandler } = require("./utils/error.handler");
 
-app.get("/", (req, res) => {
-  res.send(`Hello there!!`);
-});
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(errorHandler);
 
 console.log(` this is ${process.env.NODE_ENV}`);
 module.exports = {
