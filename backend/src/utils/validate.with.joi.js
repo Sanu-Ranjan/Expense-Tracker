997c72897abc99106aa30369375e58c7d513b3b3
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-24 14:07:26 +0530

    add data validation schemas

--- backend/src/utils/validate.with.joi.js
+++ backend/src/utils/validate.with.joi.js
@@ -1,9 +1,39 @@
 const Joi = require("joi");
 
-function validate(schema, valueObject) {
-  const { error, value } = Joi.object(schema).validate(valueObject);
-  return { err: error, value };
-}
+const schemas = {
+  user: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+  }),
+  profile: Joi.object({
+    profileName: Joi.string().required(),
+    profilePurpose: Joi.string().required(),
+    additionalInfo: Joi.string().required(),
+    userId: Joi.number().required(),
+  }),
+  accounts: Joi.object({
+    bankDetails: Joi.string().required(),
+    openingBalance: Joi.number().default(0.0),
+    profileId: Joi.number().required(),
+    userId: Joi.number().required(),
+  }),
+  ToDos: Joi.object({
+    task: Joi.string().required(),
+    date: Joi.date().required(),
+    profileId: Joi.number().required(),
+    userId: Joi.number().required(),
+  }),
+  Expenses: Joi.object({
+    date: Joi.date().required(),
+    isDebit: Joi.boolean().required(),
+    amount: Joi.number().required(),
+    description: Joi.string().required(),
+    accounts: Joi.number().required(),
+    profileId: Joi.number().required(),
+    userId: Joi.number().required(),
+  }),
+};
+
 module.exports = {
-  validate,
+  schemas,
 };
