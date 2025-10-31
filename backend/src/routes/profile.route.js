317ac6ccb6e5ae7fd71337ba8705b92a79279e04
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-10-31 23:45:43 +0530

    added routes

--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -2,6 +2,8 @@ const {
   getProfiles,
   getProfileById,
   addProfile,
+  updateProfile,
+  deleteProfile,
 } = require("../controllers/profile.controller");
 
 const router = require("express").Router();
@@ -9,7 +11,8 @@ const router = require("express").Router();
 router.post("/", addProfile);
 router.get("/", getProfiles);
 router.get("/:profileId", getProfileById);
-
+router.put("/:profileId", updateProfile);
+router.delete("/:profileId", deleteProfile);
 module.exports = {
   router,
 };
